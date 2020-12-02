42c9c3aacfebfc77505537107d441881ad530e93
Janko Dedic <jankodedic7@gmail.com>
2020-12-02 10:37:52 +0100

    Dodaj stranice za kurseve

--- scripts/kurs.js
+++ scripts/kurs.js
@@ -1,1 +1,21 @@
-console.log('Stranica kursa');
+$(function () {
+  $("#accordion").accordion();
+
+  const wishlistButton = document.getElementById("wishlist");
+  const initialBackground = wishlistButton.style.backgroundColor;
+
+  let addedToWishlist = false;
+
+  $("#wishlist").click(function () {
+    if (!addedToWishlist) {
+      this.childNodes[0].nodeValue = "Ukloni iz liste želja";
+      this.style.backgroundColor = "transparent";
+      this.style.color = "white";
+    } else {
+      this.childNodes[0].nodeValue = "Dodaj u listu želja";
+      this.style.backgroundColor = initialBackground;
+      this.style.color = "black";
+    }
+    addedToWishlist = !addedToWishlist;
+  });
+});
