c46b471edadc63003a80a0b64d76085771383443
Janko Dedic <jankodedic7@gmail.com>
2020-12-08 10:08:03 +0100

    Integrisi Jovanov kod

---
+++ scripts/index.js
@@ -0,0 +1,270 @@
+var html = "hidden";
+var css = "hidden";
+var js = "hidden";
+
+var done = true;
+
+var flag = true;
+
+$(document).ready(function(){
+
+  if ($(window).width() > 900) {
+    flag = true;
+    $(".flexbox-container").css("flex-direction", "row");
+    $(".grid-container").css("justify-content", "space-evenly");
+  }else{
+    flag = false;
+    $(".flexbox-container").css("flex-direction", "column");
+    $(".grid-container").css("justify-content", "initial");
+  }
+
+  $("body,html").css("visibility", "visible");
+
+  $(window).resize(function() {
+    if ($(window).width() > 900) {
+      if(flag != true){
+        $(".flexbox-container").css("flex-direction", "row");
+        $(".grid-container").css("justify-content", "space-evenly");
+        flag = true;
+      if(html != "hidden"){
+      $(".course-content").show();
+      $(".html-content").show();
+      $("#invisible-html").hide();
+    }else if(css != "hidden"){
+      $(".course-content").show();
+      $(".css-content").show();
+      $("#invisible-css").hide();
+    }else if(js != "hidden"){
+      $(".course-content").show();
+      $(".js-content").show();
+      $("#invisible-js").hide();
+    }
+    }
+   }else {
+    if(flag != false){
+      $(".flexbox-container").css("flex-direction", "column");
+      $(".grid-container").css("justify-content", "initial");
+
+      $(".course-content").hide();
+      $(".html-content").hide();
+      $(".css-content").hide();
+      $(".js-content").hide();
+
+      if(html != "hidden"){
+        $("#invisible-html").show();
+        if(flag != false){
+          scrollToHTML(); 
+        }
+      }else if(css != "hidden"){
+        $("#invisible-css").show();
+        if(flag != false){
+          scrollToCSS();
+        }
+      }else if(js != "hidden"){
+        $("#invisible-js").show();
+        if(flag != false){
+          scrollToJS();
+        }
+      }
+      flag = false;
+    }
+   }
+  });
+
+  // ========================= scrolling functions ====================================
+  scrollToHTML=function() {
+    $("body,html").animate({scrollTop: $("body,html").offset().top}, 800);
+  }
+
+  scrollToCSS=function() {
+    $("body,html").animate({scrollTop: $("#css-container").offset().top - 50}, 800);
+  }
+
+  scrollToJS=function() {
+    $("body,html").animate({scrollTop: $("#js-container").offset().top}, 800);
+  }
+  //================================================================================
+
+  $("#html-container").click(function(){
+    if(flag == true){
+    if(html == "hidden" && css == "hidden" && js == "hidden" && done == true){
+      done = false;
+      $(".course-content").show("slow","linear", function(){
+        $(".html-content").show("slow","linear", function(){
+          done = true;
+        });
+      });
+      html = "shown";
+    }else if(css != "hidden" && done == true){
+      done = false;
+      $(".css-content").hide("slow", "linear", function(){
+        $(".html-content").show("slow", "linear", function(){
+          done = true;
+        });
+      });
+      html = "shown";
+      css = "hidden";
+    }else if(js != "hidden" && done == true){
+      done = false;
+      $(".js-content").hide("slow", "linear", function(){
+        $(".html-content").show("slow", "linear", function(){
+          done = true;
+        });
+      });
+      html = "shown";
+      js = "hidden";
+    }
+  }else{
+    if(html == "hidden" && css == "hidden" && js == "hidden" && done == true){
+      done = false;
+      $("#invisible-html").show("slow","linear", function () {
+        done = true;
+         scrollToHTML();
+      });
+      html = "shown";
+    }else if(css != "hidden" && done == true){
+      done = false;
+      $("#invisible-css").hide("slow", "linear", function(){
+        $("#invisible-html").show("slow", "linear", function(){
+          done = true;
+          scrollToHTML();
+        });
+      });
+      html = "shown";
+      css = "hidden";
+    }else if(js != "hidden" && done == true){
+      done = false;
+      $("#invisible-js").hide("slow", "linear", function(){
+        $("#invisible-html").show("slow", "linear", function(){
+          done = true;
+          scrollToHTML();
+        });
+      });
+      html = "shown";
+      js = "hidden";
+    }
+  }
+  });
+
+  $("#css-container").click(function(){
+    if(flag == true){
+    if(html == "hidden" && css == "hidden" && js == "hidden" && done == true){
+      done = false;
+      $(".course-content").show("slow","linear", function(){
+        $(".css-content").show("slow","linear", function(){
+          done = true;
+        });
+      });
+      css = "shown";
+    }else if(html != "hidden" && done == true){
+      done = false;
+      $(".html-content").hide("slow", "linear", function(){
+        $(".css-content").show("slow", "linear", function(){
+          done = true;
+        });
+      });   
+      css = "shown";
+      html = "hidden";
+    }else if(js != "hidden" && done == true){
+      done = false;
+      $(".js-content").hide("slow", "linear", function(){
+        $(".css-content").show("slow", "linear", function(){
+          done = true;
+        });
+      });
+      css = "shown";
+      js = "hidden";
+    }
+  }else{
+    if(html == "hidden" && css == "hidden" && js == "hidden" && done == true){
+      done = false;
+        $("#invisible-css").show("slow","linear", function(){
+          scrollToCSS();
+          done = true;
+        });
+      css = "shown";
+    }else if(html != "hidden" && done == true){
+      done = false;
+      $("#invisible-html").hide("slow", "linear", function(){
+        $("#invisible-css").show("slow", "linear", function(){
+          done = true;
+          scrollToCSS();
+        });
+      });   
+      css = "shown";
+      html = "hidden";
+    }else if(js != "hidden" && done == true){
+      done = false;
+      $("#invisible-js").hide("slow", "linear", function(){
+        $("#invisible-css").show("slow", "linear", function(){
+          done = true;
+          scrollToCSS();
+        });
+      });
+      css = "shown";
+      js = "hidden";
+    }
+  }
+  });
+
+  $("#js-container").click(function(){
+    if(flag == true){
+    if(html == "hidden" && css == "hidden" && js == "hidden" && done == true){
+      done = false;
+      $(".course-content").show("slow","linear", function(){
+        $(".js-content").show("slow","linear", function(){
+          done = true;
+        });
+      });
+      js = "shown";
+    }else if(html != "hidden" && done == true){
+      done = false;
+      $(".html-content").hide("slow", "linear", function(){
+        $(".js-content").show("slow", "linear", function(){
+          done = true;
+        });
+      });
+      js = "shown";
+      html = "hidden";
+    }else if(css != "hidden" && done == true){
+      done = false;
+      $(".css-content").hide("slow", "linear", function(){
+        $(".js-content").show("slow", "linear", function(){
+          done = true;
+        });
+      });
+      js = "shown";
+      css = "hidden";
+    }
+  }else{
+    if(html == "hidden" && css == "hidden" && js == "hidden" && done == true){
+      done = false;
+        $("#invisible-js").show("slow","linear", function(){
+          scrollToJS();
+          done = true;
+        });
+      js = "shown";
+    }else if(html != "hidden" && done == true){
+      done = false;
+      $("#invisible-html").hide("slow", "linear", function(){
+        $("#invisible-js").show("slow", "linear", function(){
+          done = true;
+          scrollToJS();
+        });
+      });
+      js = "shown";
+      html = "hidden";
+    }else if(css != "hidden" && done == true){
+      done = false;
+      $("#invisible-css").hide("slow", "linear", function(){
+        $("#invisible-js").show("slow", "linear", function(){
+          done = true;
+          scrollToJS();
+        });
+      });
+      js = "shown";
+      css = "hidden";
+    }
+  }
+  });
+});
--- scripts/kupovina.js
+++ scripts/kupovina.js
@@ -1,1 +1,70 @@
-console.log('Stranica za kupovinu');
+$(document).ready(function(){
+    $("#submit").click(function(){
+        if($("#fname").val() && $("#lname").val() && $("#phone").val() && $("#email").val()){
+            if($("#html").prop("checked") != true && $("#css").prop("checked") != true && $("#js").prop("checked") != true){
+                alert("Izaberite barem jedan od ponudjenih kurseva!");
+            }else{
+                if(checkEmail($("#email").val()) != true){
+                    alert("Uneli ste neispravan e-mail!");
+                }
+                if(checkPhone($("#phone").val()) != true){
+                    alert("Uneli ste neispravan broj telefona!");
+                }
+            }
+        }
+    });
+
+    // == check functions == //
+
+    checkPhone = function(phone){
+        var PhoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+        return PhoneRegex.test(phone);
+    }
+
+    checkEmail = function(email){
+        var EmailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return EmailRegex.test(email);
+    }
+
+    // === total === //
+
+    var total = 0;
+
+    var html = 5900;
+    var css = 6900;
+    var js = 6900;
+
+    $("#html").click(function(){
+        if($("#html").prop("checked") == true){
+            total = total + html;
+        }else {
+            total = total - html;
+        }
+        upadte();
+    });
+
+    $("#css").click(function(){
+        if($("#css").prop("checked") == true){
+            total = total + css;
+            console.log(total);
+        }else {
+            total = total - css;
+        }
+        upadte();
+    });
+
+    $("#js").click(function(){
+        if($("#js").prop("checked") == true){
+            total = total + js;
+        }else {
+            total = total - js;
+        }
+        upadte();
+    });
+
+    upadte=function(){
+        $("#sum").text("TOTAL: " + total + "RSD");
+    };
+
+});
+
